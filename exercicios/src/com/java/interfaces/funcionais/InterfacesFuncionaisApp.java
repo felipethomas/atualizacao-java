bb33908824af68d9257822e88b10a85ee0d2b849
Felipe Thomas <felipe.andrade@serpro.gov.br>
2019-09-09 15:00:00 -0300

    exercícios do capítulo 4 do livro Java 8

--- exercicios/src/com/java/interfaces/funcionais/InterfacesFuncionaisApp.java
+++ exercicios/src/com/java/interfaces/funcionais/InterfacesFuncionaisApp.java
@@ -1,10 +1,57 @@
 package com.java.interfaces.funcionais;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import com.java.lambda.Usuario;
+
 public class InterfacesFuncionaisApp {
 
 	public static void main(String[] args) {
 		InterfacesFuncionaisApp interf = new InterfacesFuncionaisApp();
-		interf.ex5();
+		interf.ex8();
+	}
+	
+	void ex9() {
+		Predicate<Usuario> predicado = new Predicate<Usuario>() {
+			public boolean test(Usuario u) {
+				return u.getPontos() > 160;
+			}
+		};
+		
+		ArrayList<Usuario> usuarios = ex();
+		
+		usuarios.removeIf(predicado);
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+	
+	void ex8() {
+		ArrayList<Usuario> usuarios = ex();
+		
+		Consumer<Usuario> mostraMensagem = u -> 
+			System.out.println("antes de imprimir os nomes");
+		
+		Consumer<Usuario> imprimeNome = u ->
+			System.out.println(u.getNome());
+		
+		usuarios.forEach(mostraMensagem.andThen(imprimeNome));
+	}
+
+	void ex7() {
+		int numero = 5;
+		
+		new Thread(() -> {
+			System.out.println(numero);
+		}).start();
+	}
+	
+	void ex6() {
+		final int numero = 5;
+		
+		new Thread(() -> {
+			System.out.println(numero);
+		}).start();
 	}
 	
 	void ex5() {
@@ -50,5 +97,20 @@ public class InterfacesFuncionaisApp {
 			}
 		};
 		new Thread(r).start();
+	}
+	
+	@SuppressWarnings("serial")
+	private ArrayList<Usuario> ex() {
+		Usuario user1 = new Usuario("Paulo Silveira", 150);
+		Usuario user2 = new Usuario("Rodrigo Turini", 120);
+		Usuario user3 = new Usuario("Guilherme Silveira", 190);
+
+		return new ArrayList<Usuario>() {
+			{
+				add(user1);
+				add(user2);
+				add(user3);
+			}
+		};
 	}
 }
