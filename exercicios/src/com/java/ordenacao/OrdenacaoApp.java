8a3455a13a5fc5ce612b0784fcffd855a58575ca
Felipe Thomas <felipe.andrade@serpro.gov.br>
2019-09-10 11:26:06 -0300

    exercícios do capítulo 4 do livro Java 8

---
+++ exercicios/src/com/java/ordenacao/OrdenacaoApp.java
@@ -0,0 +1,111 @@
+package com.java.ordenacao;
+
+import java.util.Arrays;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.java.lambda.Usuario;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
+
+public class OrdenacaoApp {
+
+	public static void main(String[] args) {
+		OrdenacaoApp app = new OrdenacaoApp();
+		app.ex10();
+	}
+	
+	void ex10() {
+		List<Usuario> usuarios = ex();
+
+		usuarios.sort(comparingInt(u -> u.getPontos()));
+		usuarios.forEach(u -> System.out.println(u.getPontos()));
+	}
+	
+	void ex9() {
+		List<String> palavras =
+				Arrays.asList("Casa do Código", "Alura", "Caelum");
+
+		palavras.sort(Comparator.naturalOrder());
+		palavras.forEach(p -> System.out.println(p));
+	}
+
+	void ex8() {
+		List<Usuario> usuarios = ex();
+
+		usuarios.sort(comparing(u -> u.getNome()));
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex7() {
+		List<Usuario> usuarios = ex();
+
+		usuarios.sort(Comparator.comparing(u -> u.getNome()));
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex6() {
+		List<Usuario> usuarios = ex();
+
+		Comparator<Usuario> comparador = Comparator.comparing(u -> u.getNome());
+		usuarios.sort(comparador);
+
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex5() {
+		List<Usuario> usuarios = ex();
+
+		usuarios.sort((o1, o2) -> o1.getNome().compareTo(o2.getNome()));
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex4() {
+		List<Usuario> usuarios = ex();
+
+		Collections.sort(usuarios, (o1, o2) -> String.CASE_INSENSITIVE_ORDER.compare(o1.getNome(), o2.getNome()));
+
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex3() {
+		List<Usuario> usuarios = ex();
+
+		Collections.sort(usuarios, (o1, o2) -> o1.getNome().compareTo(o2.getNome()));
+
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex2() {
+		Comparator<Usuario> comparador = (o1, o2) -> o1.getNome().compareTo(o2.getNome());
+
+		List<Usuario> usuarios = ex();
+		Collections.sort(usuarios, comparador);
+
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	void ex1() {
+		Comparator<Usuario> comparador = new Comparator<Usuario>() {
+			public int compare(Usuario o1, Usuario o2) {
+				return o1.getNome().compareTo(o2.getNome());
+			}
+		};
+
+		List<Usuario> usuarios = ex();
+		Collections.sort(usuarios, comparador);
+
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+
+	private List<Usuario> ex() {
+		Usuario user1 = new Usuario("Paulo Silveira", 150);
+		Usuario user2 = new Usuario("Rodrigo Turini", 120);
+		Usuario user3 = new Usuario("Guilherme Silveira", 190);
+
+		return Arrays.asList(user1, user2, user3);
+	}
+
+}
