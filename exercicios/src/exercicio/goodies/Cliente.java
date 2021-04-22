082e0e3475e1bf411a2e69dc2066313fc3cbf10f
Felipe Thomas <felipe.andrade@serpro.gov.br>
2021-04-21 21:53:02 -0300

    exercícios do capítulo 11 do livro Java 8

---
+++ exercicios/src/exercicio/goodies/Cliente.java
@@ -0,0 +1,20 @@
+package exercicio.goodies;
+
+public class Cliente {
+
+	private String nome;
+
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String toString() {
+		return this.nome;
+	}
+
+}
